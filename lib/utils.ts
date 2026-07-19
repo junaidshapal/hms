efb7adb4c2d3fb8519d9b036e39c8c19a256e516
junaidshapal <mughaljunaidmughal136@gmail.com>
2026-07-20 04:19:55 +0500

    docs: add short comment to cn() helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Merges Tailwind classes, resolving conflicts (last one wins)
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
